53f934962827ce7b276ff3127670ea2a0990af2a
aylincsknpnr <aylin_comu@hotmail.com>
2021-11-11 01:43:38 +0300

    Jetpack Navigation Controller Example

---
+++ NavigationKotlin/app/src/main/java/com/sensai/navigationkotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sensai.navigationkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navigationController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navigationController= navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController, null)
+    }
+}
